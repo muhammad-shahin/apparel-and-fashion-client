1c7227163f7d500c784ff75623187ca8e19737fc
muhammad-shahin <muhammadshahin002@gmail.com>
2023-10-18 09:24:08 +0600

    banner section added

--- public/clipboard.js
+++ public/clipboard.js
@@ -1,3 +1,6 @@
 /**
- * 
+ * className='text-[48px] font-medium'
+ * style={{ fontFamily: 'DreamAvenue' }}
+ * style={{ fontFamily: 'Quicksand' }}
+ * className='flex justify-center items-center gap-4'
  */
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+
+import Banner from '../../Components/Banner/Banner';
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
